014418875cdc603c0ed12a65853ac42b60f40387
Shipra Sinha <shipra.juet@gmail.com>
2015-12-17 17:11:40 +0530

    Display the weather in forecast page

--- app.js
+++ app.js
@@ -36,8 +36,14 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
    
 }]);
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService) {
    
    $scope.city = cityService.city;
    
+ $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {
+        callback: "JSON_CALLBACK"},{ get: { method: "JSONP" }});
+    
+ $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2, appid: 'e874f3c217b0b5b04010a43e8d1d93f6' });
+        
+   console.log($scope.weatherResult);     
 }]);
